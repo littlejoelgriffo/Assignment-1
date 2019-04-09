36370b1b6629f0ffbf8aa965a7dbe5b041795a74
littlejoelgriffo <littlejoelgriffo@users.noreply.github.com>
2019-04-09 14:01:08 +1000

    Found some of the code from the last week

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
 
-int main() {
-  
-  char l;
-  scanf("%c", &l);
-  printf("\nthis is your letter:%c", l);
-  
-  return 0;
-  
+
+
+#include <stdio.h> 
+int main () { 
+    char test; 
+    printf("Enter a letter for testing: "); 
+    scanf("%c", &test); 
+    printf("\nNow this is your letter +1: %c", test+1); 
+    //printf("\n %c", 'A'+1); \\little quotes '' correspond to chars, "" to strings 
+     
+    char message [4], cm; //is the value how many characters/numbers the array message can hold? YES 
+    int i, key;  
+     
+    /*printf("\nEnter a message (4 letters) to encrypt: "); 
+    gets(message); //does this set the letters to values in the array? How do we know that they are 1-26? why does gets not work for me but does with other similar code? 
+     
+    printf("\nEnter key: "); 
+    scanf("%d", &key); */ 
+    
+    return 0; 
+     
 }
