176d7c209ce6fe380e4614bbf7b52ddf5916a735
Gabe Halpin <littlejoelgriffo@gmail.com>
2019-04-14 02:28:39 +0000

    Don't know how to get modulus 26 to work.

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 
-
-
 #include <stdio.h> 
 int main () { 
     char test; 
     printf("Enter a letter for testing: "); 
     scanf("%c", &test); 
-    //test to see if this commit on this fine sunday works
-    printf("\nNow this is your letter +1: %c", test+1); 
-    //printf("\n %c", 'A'+1); \\little quotes '' correspond to chars, "" to strings 
+    test=(test+1) %26;
+    printf("\nNow this is your letter +1: %c", test); 
+    /*printf("\n %c", 'A'+1); \\little quotes '' correspond to chars, "" to strings 
      
-    char message [4], cm;
+    char message[4], cm;
     int i, key;  
+    cm=i=key;
      
-    /*printf("\nEnter a message (4 letters) to encrypt: "); 
+    printf("\nEnter a message (4 letters) to encrypt: "); 
     gets(message); //does this set the letters to values in the array? How do we know that they are 1-26? why does gets not work for me but does with other similar code? 
      
     printf("\nEnter key: "); 
