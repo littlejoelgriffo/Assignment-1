9086d1edc5781ac31ca0ac89fc3ecac51a03495f
littlejoelgriffo <littlejoelgriffo@users.noreply.github.com>
2019-04-29 16:54:23 +1000

    Update main.c
    Some comments

--- main.c
+++ main.c
@@ -1,35 +1,42 @@
+/* This program was created by Gabe Halpin c3302926. It works with a hardcoded message and can encrypt/decrypt
+via substitution or rotation with a key given and for a rotation ciphered text, decrypt it without the key given to the program.
+To change the message, see the second line of the int main. To change the substitution key, see the switch case in the
+respective encrypt and decrypt functions. The key for rotation will be prompted for the user to scan in each time. */
+
 #include<stdio.h>
-#include<unistd.h>
-char lower2upper(char Jon);
-char sub_boi(char Jon);
-char sub_boi_d(char Jon);
-char rotor_boi(char Jon, int key);
-char rotor_boi_d(char Jon, int key);
-char kingrotor_boi(char *msg, int ls, int debug);
+#include<unistd.h> //This is to allow the sizeof command to work.
+char lower2upper(char Jon); //Converts any present lower case letters in the message to upper case
+char sub_boi(char Jon); //Substitutes given letters in the alphabet for others to create encrypted text.
+char sub_boi_d(char Jon); //Substitutes given letters in the alphabet for others to decrypt text.
+char rotor_boi(char Jon, int key); //Rotates letters by a certain number on the alphabet (key) to encrypt text.
+char rotor_boi_d(char Jon, int key); //Rotates letters by a certain number on the alphabet (key) to decrypt text.
+char kingrotor_boi(char *msg, int ls, int debug); //Decrypts a rotation cipher encrypted message by making assumptions on english words included.
 int main()
 {
     int debug = 0;              //This is to switch on and off some extra printfs that in the case of things going wrong may make the problem clearer to see. Change to =1 to enable.
-    char msg[] = "ABCD";
-    int ls = sizeof(msg) - 1;
-    int i;                      // sets 
-    //printf("%d", ls);
-    /* Convert any lower case  letters to upper case  */
-    /****/ for (int i = 0; i < ls; i++) {
-        msg[i] = lower2upper(msg[i]);
+    char msg[] = "SJSFMPCRM WG O USBWIG. PIH WT MCI XIRUS O TWGV PM WHG OPWZWHM HC QZWAP O HFSS, WH KWZZ ZWJS WHG KVCZS ZWTS PSZWSJWBU HVOH WH WG GHIDWR. - OZPSFH SWBGHSWB"; 
+    //^This is the message passed on the respective functions. Change to whatever you would like to encrypt/decrypt.
+    int ls = sizeof(msg) - 1; //This is to find the length of the string holding the message. I have found the sizeof function gets a value that is one too high so the -1 fixes that.
+    //^ ls is very important for dictating when to end loops used throughout this program.
+    int i;   // used for incrementing place in arrays. 
+    if (debug==1) 
+        printf("%d", ls); //This will print the value of ls in the case that loops are not going for the correct amount of iterations.
+    
+    for (int i = 0; i < ls; i++) {    //Increments the place in the array so one by one all letters are passed into the function.
+        msg[i] = lower2upper(msg[i]); //calls the lowercase to uppercase converter function and stores the values to overwrite the given message.
 
-    /****/ }
-    /* Converted any lower case letters to upper case */
+    }
+    
 
-    // PUT IN MENU HERE
-    int choice;
-    while (choice != 666) {
+    //This is the menu:
+    int choice; //Used to hold the value of what the user enters into the menu.
+    while (choice != 666) {  //Loops the menu so the user can test multiple functions in one run. Exits when the user enters 666.
         printf
             ("\n->Enter 1 for Rotation Encryption given key\n2 for Rotation Decryption given key\n3 for Substitution Encryption (key given)\n4 for Subsitution Decryption (key given)\n5 for Rotation Decryption without key\n->Enter 666 to exit \n");
-        scanf("%d", &choice);
+        scanf("%d", &choice); //scans in the value the user enters which then dictates which switch case is activated.
 
-        //Put menu here
         switch (choice) {
-        case 1:;               //Case for if rotation is chosen
+        case 1:;               //Case for if rotation encryption is chosen
             int key = 0;
             printf
                 ("Enter a number between 1 and 25 for the rotation key: ");
@@ -49,11 +56,11 @@ int main()
                 printf("%c", rotor_boi_d(msg[i], key));
             }
             break;
-        case 3:                //Case for if substitution is chosen
+        case 3:                //Case for if substitution encryption is chosen
             for (int i = 0; i < ls; i++) {
                 printf("%c", sub_boi(msg[i]));
             } break;
-        case 4:                //Case for if substitution is chosen
+        case 4:                //Case for if substitution decryption is chosen
             for (int i = 0; i < ls; i++) {
                 printf("%c", sub_boi_d(msg[i]));
             } break;
@@ -76,8 +83,8 @@ char lower2upper(char Jon)
 char sub_boi(char Jon)
 {
     switch (Jon) {
-    case 'A': Jon = 'Z';break;
-        case 'B': Jon = 'Y';break;
+        case 'A': Jon = 'Z';break; //I know this is an exception to my indenting style, however it makes my file 100+ lines less which is a lot..
+        case 'B': Jon = 'Y';break; //.. easier to read and navigate. Please don't make me down for this.
         case 'C': Jon = 'X';break;
         case 'D': Jon = 'W';break;
         case 'E': Jon = 'V';break;
@@ -266,8 +273,8 @@ char rotor_boi_d(char Jon, int key)
 char sub_boi_d(char Jon)
 {
     switch (Jon) {
-    case 'A': Jon = 'Z';break;
-        case 'B': Jon = 'Y';break;
+        case 'A': Jon = 'Z';break; //I know this is an exception to my indenting style, however it makes my file 100+ lines less which is a lot..
+        case 'B': Jon = 'Y';break; //.. easier to read and navigate. Please don't make me down for this.
         case 'C': Jon = 'X';break;
         case 'D': Jon = 'W';break;
         case 'E': Jon = 'V';break;
